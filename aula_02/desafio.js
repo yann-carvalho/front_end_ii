0bac2a15ed4593a72bf4944800ea637197e35cf0
Yann Carvalho <yanncarvalho95@gmail.com>
2023-08-31 19:45:17 -0300

    js

--- aula_02/desafio.js
+++ aula_02/desafio.js
@@ -1,3 +1,9 @@
+// Cálculo de média ponderada com 3 notas, sendo a n1 com peso 2, n2 com peso 3 e n3 com peso 5
+
 var n1 = parseFloat(prompt("Informe a primeira nota: "));
 var n2 = parseFloat(prompt("Informe a segunda nota: "));
 var n3 = parseFloat(prompt("Informe a terceira nota: "));
+
+nota = ((n1*2)+(n2*3)+(n3*5)) / (2+3+5)
+
+console.log("A média ponderada do aluno é: ", nota)
